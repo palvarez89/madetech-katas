5aa3e7f1e44c5b60cc8727c59a3c304c7415911b
Pedro Alvarez <pedro@alvarezpiedehierro.com>
2024-07-24 17:14:55 +0100

    Main wordle loop

--- wordle/main.py
+++ wordle/main.py
@@ -8,3 +8,16 @@ while len(word) != 5:
 
 word = word.upper()
 print(word)
+
+attempts = 0
+
+guess = ""
+while word != guess:
+    guess = input("Introduce guess: ")
+    if len(guess) != 5:
+        print("Guess needs to be exactly 5 chars long.")
+        continue
+    guess = guess.upper()
+
+
+print("You guessed it!!")
